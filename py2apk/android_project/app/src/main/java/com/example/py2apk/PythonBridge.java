decdb34c36ae8a4aaa95abcba1e2d58017276170
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-01-30 02:36:45 -0600

    Update PythonBridge.java

--- py2apk/android_project/app/src/main/java/com/example/py2apk/PythonBridge.java
+++ py2apk/android_project/app/src/main/java/com/example/py2apk/PythonBridge.java
@@ -1,3 +1,29 @@
 package com.example.py2apk;
 
-import com.chaquo.p
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class PythonBridge {
+    private static PythonBridge instance;
+    private Python python;
+
+    private PythonBridge() {
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(context));
+        }
+        python = Python.getInstance();
+    }
+
+    public static PythonBridge getInstance() {
+        if (instance == null) {
+            instance = new PythonBridge();
+        }
+        return instance;
+    }
+
+    public String runPythonScript(String scriptName, String input) {
+        PyObject pyObject = python.getModule(scriptName);
+        return pyObject.callAttr("process_input", input).toString();
+    }
+}
