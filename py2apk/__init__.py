e8c00f4519ae2ee41929a8505db36d0f2d7ed9df
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-01-31 03:07:40 -0600

    Update __init__.py

--- py2apk/__init__.py
+++ py2apk/__init__.py
@@ -1,2 +1,3 @@
-# Py2APK package initialization
-__version__ = "0.1.0"
+"""Py2APK package initialization"""
+__version__ = "1.0.0"  # Package version
+__author__ = "Your Name <your.email@example.com>"  # Maintainer info
